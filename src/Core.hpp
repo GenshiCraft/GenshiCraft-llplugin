0a030a53f97e3400c834e83b9de943d304e9424b
Futrime <futrime@outlook.com>
2022-07-22 15:36:17 +0800

    Add license

---
+++ src/Core.hpp
@@ -0,0 +1,29 @@
+/**
+ *    GenshiCraft. Play Genshin Impact in Minecraft!
+ *    Copyright (C) 2022  Futrime <futrime@outlook.com>
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as published
+ *    by the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+/**
+ * @file Core.hpp
+ * @author Futrime (futrime@outlook.com)
+ * @brief Definition of the Core class
+ * @version 1.0.0
+ * @date 2022-07-21
+ * 
+ * @copyright Copyright (c) 2022 Futrime
+ * 
+ */
